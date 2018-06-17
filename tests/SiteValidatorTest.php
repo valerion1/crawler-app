b43c0dea966f81a0aa50b28d5c49ab6e64528430
Valeriy <valeriy.nezhuriov@gmail.com>
2018-06-17 19:41:29 +0300

    add SiteValidatorTest

---
+++ tests/SiteValidatorTest.php
@@ -0,0 +1,26 @@
+<?php declare( strict_types = 1 );
+
+namespace Tests;
+
+use App\Validation\SiteValidator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SiteValidatorTest
+ * @package Tests
+ */
+class SiteValidatorTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testIsValid () : void
+    {
+        self::assertFalse((new SiteValidator('htpp://example.com'))->isValid());
+        self::assertFalse((new SiteValidator('https://example,com'))->isValid());
+        self::assertFalse((new SiteValidator('https:/example.com'))->isValid());
+        self::assertFalse((new SiteValidator('https:/example'))->isValid());
+        self::assertTrue((new SiteValidator('https://example.com.ua'))->isValid());
+        self::assertTrue((new SiteValidator('https://sub.example.com'))->isValid());
+    }
+}
